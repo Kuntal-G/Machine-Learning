704056cc9b73271fec5274ffb1fa09e6b2e4c2c2
Kuntal Ganguly <gangulykuntal1986@gmail.com>
2015-05-02 19:47:45 +0600

    ComputeCost updated

--- Coursera-008/mlclass-ex1-008/mlclass-ex1/computeCost.m
+++ Coursera-008/mlclass-ex1-008/mlclass-ex1/computeCost.m
@@ -9,7 +9,7 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
+% ====================== YOUR  CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
